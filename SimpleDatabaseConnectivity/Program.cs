1738839bbdbbd2cfb52b88f9103314b658882049
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2022-12-06 15:23:24 +0530

    dotnet

---
+++ SimpleDatabaseConnectivity/Program.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+string conStr="server=localhost; uid=root;password=PASSWORD;database=classicmodels";
+MySqlConnection con=new MySqlConnection();
+try{
+    con.ConnectionString=conStr;
+    con.Open();
+    string query ="select * from employees";
+    MySqlCommand cmd = new MySqlCommand(query,con);
+    MySqlDataReader rdr =cmd.ExecuteReader();
+
+    while (rdr.Read())
+    {
+        Console.WriteLine(rdr["employeeNumber"]+"--"+rdr["lastName"]+"--"+rdr["firstName"]+"--"+rdr["email"]);
+    }
+    rdr.Close();
+}
+catch(Exception ee) {
+    Console.WriteLine(ee.Message);
+}
+finally{
+    con.Close();
+}
