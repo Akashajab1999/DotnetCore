c34022aca3e008ae3a64ed97cc1c301510947608
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2022-12-27 21:52:39 +0530

    MongoDBTestApp

---
+++ MongoDB/MongoDBTestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBTestApp{
+
+
+    public class Student{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id{get; set ;}
+        public string name{get ; set;}
+        public string LastName{get ;set;}
+        
+    }
+
+    class program{
+        public static void Main(string[] args){
+            List<Student> list =new List<Student>();
+            var _mongoClient =new MongoClient("mongodb://localhost:27017");
+            var db=_mongoClient.GetDatabase("transflower");
+            var collection =db.GetCollection<Student>("students");
+            collection.Find(_=>true).ToList().ForEach(
+                Students=>{
+                    Console.WriteLine(Students.name);
+                    Console.WriteLine(Students.LastName);
+                    
+                }
+            );
+        }
+    }
+}
