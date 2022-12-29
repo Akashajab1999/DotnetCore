e74bc5f02fd19d09d345e2781a05718073b88fbc
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2022-12-29 17:30:59 +0530

    using delegate

--- Delegate/HandlerTestApp/Program.cs
+++ Delegate/HandlerTestApp/Program.cs
@@ -8,9 +8,20 @@ namespace Banking{
     static void Main(string[] args){
 
         Handler theHandler=new Handler();
-        theHandler.BlockedAccount();
-        theHandler.SendEmailNotification();
-        theHandler.PayIncomeTax();
+        //theHandler.BlockedAccount();
+        //theHandler.SendEmailNotification();
+        //theHandler.PayIncomeTax();
+
+        AccountOperation operation1=new AccountOperation(theHandler.BlockedAccount);
+        
+        AccountOperation operation2=new AccountOperation(theHandler.SendEmailNotification);
+        
+        AccountOperation operation3=new AccountOperation(theHandler.PayIncomeTax);
+        AccountOperation operation = operation1;
+        operation+=operation2;
+        operation+=operation3;
+        operation();
+        Console.WriteLine("hello Akash");
     }
  }   
 }
