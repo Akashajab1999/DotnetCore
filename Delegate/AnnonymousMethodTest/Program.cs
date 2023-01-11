48079268cd73767cb44fede5a0f1a221e40eabea
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-01-11 14:29:45 +0530

    annonymous Method

---
+++ Delegate/AnnonymousMethodTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace AnnonumousMethodTest{
+
+ public  delegate void BasicOperation();
+
+    class Program{
+      //call back
+      static void PrintReport(){
+        Console.WriteLine(" Printing a report : graph,table,score ");
+      }
+
+        static void Main(string[] args){
+
+            //resister the function 
+            //registering callback function
+            BasicOperation method1= new BasicOperation(PrintReport);
+            //resolving name of the function at runtime
+            method1();
+
+
+
+            //Inline function 
+            //annonymous method
+
+            BasicOperation method2= delegate(){
+                Console.WriteLine("Printing company annual report");
+            };
+            method2();
+        }
+    }
+}
+
