d48714476e8909bdbb56f2f3bbafcd793b1ff42a
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-09-02 23:12:23 +0530

    polymorphism

---
+++ Polymorphism/polymorphism/College.cs
@@ -0,0 +1,10 @@
+public class College{
+
+
+public virtual string CollegeName(){
+
+         string  collageName="Sinhagad institute of technology lonavala";
+          return collageName;
+        }
+
+}
---
+++ Polymorphism/polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//method overloading
+
+Student student = new Student();
+
+ String Employee=student.AddName("Pragati","Ajab");
+  Console.WriteLine(Employee);
+
+string employee=student.AddName("Akash");
+ Console.WriteLine(employee);
+
+
+
+
+//method overriding
+College p1 = new Student();
+ string c1=p1.CollegeName();
+ Console.WriteLine(c1);
+College p2 = new College();
+ string c2=p2.CollegeName();
+ Console.WriteLine(c2);
+
