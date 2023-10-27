4ae1cb4b724413941e992a9a00284684629c613a
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-10-27 20:24:22 +0530

    decimal to octal

---
+++ DecimalToOctal/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+ 
+   Console.WriteLine("Enter Your Number");
+    int num =int.Parse(Console.ReadLine());
+    int octal =0;
+    int rem ,i=1;
+
+    while(num!=0){
+
+        rem=num%8;
+        num=num/8;
+        octal += rem * i;
+        i=i*10;
+    }
+    
+    Console.WriteLine(octal);
+
+
+
+
