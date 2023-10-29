54e688dae7414ca387e5353b57ea1d78f5106819
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-10-29 20:05:26 +0530

    Sum of NaturaL Numbers

---
+++ natural numbers addition/Program.cs
@@ -0,0 +1,14 @@
+﻿static int SumOfNumbersGivenRange(int number1, int number2)
+{
+    int sum = 0;
+    for (int i = number1; i <= number2; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+
+int Sum= SumOfNumbersGivenRange(12,13);
+Console.WriteLine(Sum);
