29a2390905ca82e995128f87e11c9205aeaaf35f
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-02-27 22:48:12 +0530

    MVC app

---
+++ MvcApp/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
+using TFLPortal.Models;
+using TFLPortal.Services;
+
+namespace MvcApp.Controllers;
+
+public class ProjectController : Controller
+{
+
+
+  private readonly IProjectService _projectService;
+    public ProjectController(IProjectService projectService)
+    {
+       _projectService=projectService;
+    }
+
+   
+     public IActionResult GetProject(int id)
+    {
+
+        Project project=_projectService.GetProject(id);
+        //  ViewData["Project"]=project;
+           return View(project);
+    }
+
+    
+
+
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
